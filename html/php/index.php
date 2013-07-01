cea096cdcab26b6d88275c25011e5dcc0b2e7c16
Riley Teige <riley.teige@gmail.com>
2013-06-30 23:04:19 -0700

    Shuffling images again.

--- html/php/index.php
+++ html/php/index.php
@@ -4,6 +4,8 @@ $images = array_filter(scandir('../content/images'), function($item) {
 	return !(strpos($item, ".jpg") === false) && strpos($item, ".") > 0;
 });
 
+shuffle($images);
+
 echo json_encode($images);
 
 ?>
